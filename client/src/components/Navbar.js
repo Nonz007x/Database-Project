58a6df30c5b1fe924b60b56d17790db08e37b3ae
SueaWS <witsarut1133@outlook.com>
2023-02-18 20:54:43 +0700

    add Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+import Button from "@mui/material/Button";
+
+function Navbar(){
+    return(
+        <div className="BG">
+            <Button variant="contained" size="small" color="secondary">ล็อคอินเข้าระบบ/สมัตรสมาชิก</Button>
+        </div>
+    );
+}
+export default Navbar;
