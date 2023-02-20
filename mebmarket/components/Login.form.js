9087f2cf3d7b82ce5b18b04ace2eb8f9060c563c
SueaWS <witsarut1133@outlook.com>
2023-02-20 13:15:15 +0700

    add asdasdasd

--- mebmarket/components/Login.form.js
+++ mebmarket/components/Login.form.js
@@ -3,9 +3,15 @@ import Button from "@mui/material/Button";
 import PasswordInput from './PasswordInput';
 import { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import { TextField } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 function LoginPage(){
     const [isLoginFormVisible,setIsLoginFormVisible]=useState(false);
     const handleClick=()=>{setIsLoginFormVisible(!isLoginFormVisible)}
+    const [showPassword, setShowPassword] = useState(false);
+    const [password,setPassword] = useState("");
+    const handleClickPassword=()=>{setShowPassword(!showPassword)}
     return(
         <div>
             <Button variant="contained" size="small" onClick={handleClick}>ล็อคอินเข้าสู่ระบบ/สมัครสมาชิก</Button>
@@ -14,7 +20,12 @@ function LoginPage(){
                     <div id='GotoMiddleOfTheScreen'>
                         Login
                         <CloseIcon onClick={handleClick}/>
-                        <PasswordInput/>
+                        <div id='setPasswordElementPosition'>
+                            <TextField type={showPassword? "text":"password"} value={password} onChange={(e)=>{setPassword(e.target.value)}} size="small" label="Password"/>
+                                <div>
+                                    {!showPassword?<VisibilityIcon className="eye" onClick={handleClick}/>:<VisibilityOffIcon className="eye" onClick={handleClickPassword}/>}
+                                </div>
+                        </div>
                     </div>
                 </div>
         :null}
--- mebmarket/components/PasswordInput.js
+++ mebmarket/components/PasswordInput.js
@@ -4,14 +4,12 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 export default function PasswordInput() {
     const [showPassword, setShowPassword] = React.useState(false);
-    const [password,setPassword] = React.useState("")
     const handleClick=()=>{setShowPassword(!showPassword)}
     return (
         <div id='setPasswordElementPosition'>
-            <TextField type={showPassword? "text":"password"} size="small" label="Password" 
-            value={password} onChange={e=>{setPassword(e.target.value)}}/>
+            <TextField type={showPassword? "text":"password"} size="small" label="Password"/>
             <div>
-                {!showPassword?<VisibilityIcon className="eye" onClick={handleClick}/>:<VisibilityOffIcon className="eye" onClick={handleClick}/>}
+                {!showPassword?<VisibilityIcon className="eye" onClick={handleClick}/>:<VisibilityOffIcon className="eye" onClick={handleClickPassword}/>}
             </div>
         </div>
     );
