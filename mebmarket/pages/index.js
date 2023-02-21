4830b465e8d965dcbf44e56d206cd27500c6abb9
Nonz007x <comandor969@gmail.com>
2023-02-21 23:35:33 +0700

    change parameter

--- mebmarket/pages/index.js
+++ mebmarket/pages/index.js
@@ -5,11 +5,13 @@ import '@fontsource/roboto/400.css'
 import { fetcher } from './api/fetcher'
 import { useEffect, useState } from 'react'
 
+
 export default function Home() {
   const [Data,SetData] = useState([])
   useEffect(()=>{
     fetcher('http://localhost:3000/api/get').then((e)=>{
       SetData(e);
+      console.log(Data)
     })
   },[])
   const mapping = Data.map((property,index)=>{
@@ -31,6 +33,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      
     </>
   )
 }
