571914ce858d3f2d25a39ad08e172b73bc60da4e
Nonz007x <comandor969@gmail.com>
2023-03-01 01:04:18 +0700

    add edit

--- mebmarket/pages/api/addbook.js
+++ mebmarket/pages/api/addbook.js
@@ -3,7 +3,14 @@ import excuteQuery from "@/shared/database";
 export default async function handler(req,res){
     const {bookId,bookname,author,price,cover,synopsis,date} = req.body;
     try{
-        const sql =await excuteQuery({
+        const select = await excuteQuery({
+            query: "SELECT bookId FROM `book` WHERE bookId = ?"
+            ,values:[bookId]
+        })
+        if (select.length > 0){
+            return res.status(200).json('หนังสือมีอยู่แล้ว',select);
+        }
+        const sql = await excuteQuery({
             query: "INSERT INTO `book`(`bookId`, `bookname`, `author`, `price`, `cover`, `date`, `synopsis`) VALUES (?,?,?,?,?,?,?)"
             ,values:[bookId,bookname,author,price,cover,date,synopsis]
         })
--- mebmarket/pages/api/editbook.js
+++ mebmarket/pages/api/editbook.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
                 "UPDATE `book` SET `bookname` = ?, `author` = ?, `price` = ?, `cover` = ?, `synopsis` = ?, `date` = ? WHERE `bookId` = ?",
             values: [bookname, author, price, cover, synopsis, date, bookId],
         });
-        return res.status(201).json('เพิ่มหนังสือสำเร็จ', sql);
+        return res.status(201).json('แก้ไขสำเร็จ', sql);
     } catch (error) {
         console.log(error);
         return res.status(500).json('Internal Server Error');
