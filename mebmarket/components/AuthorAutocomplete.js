37f73d3d2a9b38de30c20bb47a40a2ffd9f52927
Nonz007x <comandor969@gmail.com>
2023-03-01 00:29:38 +0700

    editable book

--- mebmarket/components/AuthorAutocomplete.js
+++ mebmarket/components/AuthorAutocomplete.js
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
 import { Autocomplete, TextField } from "@mui/material";
-import {fetcher}  from "../pages/api/fetcher"
+import { fetcher } from "../pages/api/fetcher"
 
-export default function AuthorAutocomplete(props){
-    // console.log(props)
-    const [Option,setOption] = useState([])
-    const [SelectedValue,setSelectedValue] = useState(null)
-    useEffect(()=>{
-        fetcher("http://localhost:3000/api/get").then(e=>{
-            const authorNameMapped = e.map(Object=>Object.author)
+export default function AuthorAutocomplete(props) {
+    const [Option, setOption] = useState([])
+    const [SelectedValue, setSelectedValue] = useState(null)
+    useEffect(() => {
+        fetcher("http://localhost:3000/api/get").then(e => {
+            const authorNameMapped = e.map(Obj => Obj.author)
             const authorFilter = [...new Set(authorNameMapped)]
             setOption(authorFilter)
         })
-    },[])
+    }, [])
     return (
         <>
             <Autocomplete
                 id="testAutocomplete"
-                sx={{width:300}}
+                sx={{ width: 300 }}
                 options={Option}
-                renderInput={(params)=> <TextField {...params} label="นักเขียน" size="small"/>}
+                renderInput={(params) => <TextField {...params} label="นักเขียน" size="small" onChange={e => {
+                    props.onChange(e.target.value)
+                    setSelectedValue(e.target.value)
+                }} />}
                 value={SelectedValue}
                 freeSolo
-                onChange={(e,newValue)=>{
+                onChange={(e, newValue) => {
                     props.onChange(newValue)
                     setSelectedValue(newValue)
                 }}
---
+++ mebmarket/pages/api/editbook.js
@@ -0,0 +1,15 @@
+import excuteQuery from "@/shared/database";
+export default async function handler(req, res) {
+    const { bookId, bookname, author, price, cover, synopsis, date } = req.body;
+    try {
+        const sql = await excuteQuery({
+            query:
+                "UPDATE `book` SET `bookname` = ?, `author` = ?, `price` = ?, `cover` = ?, `synopsis` = ?, `date` = ? WHERE `bookId` = ?",
+            values: [bookname, author, price, cover, synopsis, date, bookId],
+        });
+        return res.status(201).json('เพิ่มหนังสือสำเร็จ', sql);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal Server Error');
+    }
+}
