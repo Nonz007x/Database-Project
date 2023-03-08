db98445e6df400c557e20c60d96189dbf1154179
SueaWS <witsarut1133@outlook.com>
2023-03-08 10:14:41 +0700

    add update database rating

--- mebmarket/pages/api/PostcommentAndRating.js
+++ mebmarket/pages/api/PostcommentAndRating.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import excuteQuery from "@/shared/database";
-import { use } from "react";
 
 export default async function handler(req, res) {
     const newDate = new Date();
@@ -14,8 +13,14 @@ export default async function handler(req, res) {
         query: "INSERT INTO `comment` (`bookId`, `username`, `comment`,`rating`,`time`) VALUES (?,?,?,?,?);",
         values: [bookId, username, comment, rating, newDate],
     });
+    const sqlUpdate = await excuteQuery({
+        query: "UPDATE `book` SET `rating` = (SELECT AVG(rating) FROM comment WHERE bookId = ?) where book.bookId = ?",
+        values: [bookId, bookId],
+    });
     //  if (res.status !== 200) {
     //     throw new Error(res.message);
     //   }
+    console.log(sqlSelect);
+    console.log(sqlUpdate)
     res.status(200).json("hi");
 }
