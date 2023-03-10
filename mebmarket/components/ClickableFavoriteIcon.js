c90fe1956a1f743f8feb9a4b43d523f9ea60d820
SueaWS <witsarut1133@outlook.com>
2023-03-10 21:59:49 +0700

    mo favo

--- mebmarket/components/ClickableFavoriteIcon.js
+++ mebmarket/components/ClickableFavoriteIcon.js
@@ -1,20 +1,50 @@
+// import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
+// import BookmarkAddedIcon from "@mui/icons-material/BookmarkAdded";
+// import { useState } from "react";
+// export default function ClickableFavoriteIcon({value,newValue}) {
+//     const handleFavoriteClick = () => {
+//         setState(!state);
+//         newValue=state;
+//     };
+//     const [state, setState] = useState(value);
+//     return (
+//         <div className="favorite-zone-element" onClick={handleFavoriteClick}>
+//             {state ? (
+//                 <BookmarkAddedIcon className="color-var-maincolor-in-ClickableFavoriteIcon" />
+//             ) : (
+//                 <BookmarkAddOutlinedIcon className="color-var-maincolor-in-ClickableFavoriteIcon" />
+//             )}
+//             <p>อยากได้</p>
+//         </div>
+//     );
+// }
+
+
 import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
 import BookmarkAddedIcon from "@mui/icons-material/BookmarkAdded";
 import { useState } from "react";
-export default function ClickableFavoriteIcon({value,newValue}) {
-    const handleFavoriteClick = () => {
-        setState(!state);
-        newValue=state;
-    };
-    const [state, setState] = useState(value);
-    return (
-        <div className="favorite-zone-element" onClick={handleFavoriteClick}>
-            {state ? (
-                <BookmarkAddedIcon className="color-var-maincolor-in-ClickableFavoriteIcon" />
-            ) : (
-                <BookmarkAddOutlinedIcon className="color-var-maincolor-in-ClickableFavoriteIcon" />
-            )}
-            <p>อยากได้</p>
-        </div>
-    );
+
+export default function ClickableFavoriteIcon({ value, onChange }) {
+  const [isFavorite, setIsFavorite] = useState(value);
+
+  const handleFavoriteClick = () => {
+    const newFavoriteState = !isFavorite;
+    setIsFavorite(newFavoriteState);
+
+    if (onChange) {
+      onChange(newFavoriteState);
+    }
+  };
+
+  return (
+    <div className="favorite-zone-element" onClick={handleFavoriteClick}>
+      {isFavorite ? (
+        <BookmarkAddedIcon className="color-var-maincolor-in-ClickableFavoriteIcon" />
+      ) : (
+        <BookmarkAddOutlinedIcon className="color-var-maincolor-in-ClickableFavoriteIcon" />
+      )}
+      <p>อยากได้</p>
+    </div>
+  );
 }
+
