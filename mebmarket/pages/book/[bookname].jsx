dc1c52fc51c4a479bdac14279e794da73c0f9a36
FuseSN2003 <sukavit.fuse2003@gmail.com>
2023-03-15 05:45:46 +0700

    hide publisher

--- mebmarket/pages/book/[bookname].jsx
+++ mebmarket/pages/book/[bookname].jsx
@@ -210,7 +210,7 @@ export default function Page() {
                                         pathname: "/search/author/[author]",
                                         query: { author: bookData.author }
                                     }}> {bookData.author}</Link></p>
-                                    <p>สำนักพิมพ์ <a href="">//ยังไม่มีสำนักพิมพ์</a></p>
+                                    {/* <p>สำนักพิมพ์ <a href="">//ยังไม่มีสำนักพิมพ์</a></p> */}
                                     <p>หมวดหมู่ <a href=""> {bookData.categoryName}</a></p>
                                 </div>
                                 <div id="TryAndBuyDiv">
