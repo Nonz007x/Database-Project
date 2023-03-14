9e55b11197e2e0870d15447afdd923e978c91a39
Nonz007x <comandor969@gmail.com>
2023-03-15 04:02:44 +0700

    Ascended Over Heaven

--- mebmarket/pages/api/bookmap/getpage.js
+++ mebmarket/pages/api/bookmap/getpage.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const { page } = req.body;
     const { amount } = req.body;
     const sql = await excuteQuery({
-        query: "select * from `book` LEFT JOIN `category` ON book.category = category.categoryId limit ?,?",
+        query: "select * from `book` LEFT JOIN `category` ON book.category = category.categoryId ORDER BY bookId ASC LIMIT ?,?",
         values: [amount * (page - 1), Number(amount)],
     });
     res.send(sql);
