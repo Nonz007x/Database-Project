903a386a0a313e7ec64e5cedc67fdf596e17addf
Nonz007x <comandor969@gmail.com>
2023-03-17 16:29:51 +0700

    fix lowercase

--- mebmarket/pages/api/getrecentComment.js
+++ mebmarket/pages/api/getrecentComment.js
@@ -2,7 +2,7 @@ import excuteQuery from "@/shared/database";
 
 export default async function handler(req, res) {
     const Sql = await excuteQuery({
-        query: "select comment.*,bookname from comment JOIN book on book.bookId = comment.bookId ORDER BY `comment`.`time` DESC",
+        query: "SELECT comment.*,bookname FROM comment JOIN book ON book.bookId = comment.bookId ORDER BY `comment`.`time` DESC",
     });
     res.send(Sql);
 }
