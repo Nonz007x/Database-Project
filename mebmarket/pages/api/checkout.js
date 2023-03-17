d3a07feb001974699a05085a02ac72257fc3ad01
Nonz007x <comandor969@gmail.com>
2023-03-17 16:11:36 +0700

    dashboard

--- mebmarket/pages/api/checkout.js
+++ mebmarket/pages/api/checkout.js
@@ -1,59 +1,3 @@
-// import excuteQuery from "@/shared/database";
-// import mysql from 'serverless-mysql';
-
-// const db = mysql({
-//     config: {
-//         host: process.env.MYSQL_HOST,
-//         port: process.env.MYSQL_PORT,
-//         database: process.env.MYSQL_DATABASE,
-//         user: process.env.MYSQL_USER,
-//         password: process.env.MYSQL_PASSWORD
-//     }
-// });
-
-// export default async function handler(req, res) {
-//     const { SelectedItem, username, totalPrice } = req.body;
-//     const date = new Date();
-//     const length = SelectedItem.length
-//     const queries = [{
-//         query: 'INSERT INTO invoices (username, dateOrdered, totalAmount) VALUES (?, ?, ?);',
-//         values: [username, date, totalPrice],
-//     },
-//     {
-//         query: 'INSERT INTO order_items_table (orderId, bookId, price) VALUES (?,?,?)',
-//     },
-//     {
-//         query: 'DELETE FROM `cart_inventory` WHERE bookId = ? AND username = ?',
-//     },
-//     ];
-
-//     const sql = await excuteQuery({
-//         query: 'SELECT MAX(orderId) FROM invoices',
-//     })
-//     let maxOrderId = sql[0]['MAX(orderId)'];
-//     if (maxOrderId === null) {
-//         maxOrderId = 1;
-//     }
-//     let results = db.transaction().query(queries[0].query, queries[0].values)
-//     try {
-
-//         for (let i = 0; i < length; i++) {
-//             if(SelectedItem[i] !== null && SelectedItem[i] !== '' && SelectedItem[i] !== undefined) {
-//                 results = results.query(queries[1].query, [maxOrderId+1, SelectedItem[i].bookId, SelectedItem[i].price]).query(queries[2].query, [SelectedItem[i].bookId, username]);
-//             }
-//         }
-//         await results.commit();
-//     } catch (error) {
-//         results.rollback();
-//         res.status(500).json("Something went wrong")
-//         throw error;
-//     } finally {
-//         await db.end();
-//     }
-//     res.json(results)
-
-// }
-
 import mysql from 'serverless-mysql';
 
 const db = mysql({
@@ -70,8 +14,8 @@ export default async function handler(req, res) {
     const { SelectedItem, username, totalPrice } = req.body;
     const date = new Date();
     const length = SelectedItem.length;
-    const getOrderId = await db.query('SELECT MAX(orderId) FROM invoices')
-    let maxOrderId = getOrderId[0]['MAX(orderId)'];
+    const getOrderId = await db.query('SELECT MAX(orderId) AS id FROM invoices')
+    let maxOrderId = getOrderId[0].id;
     if (maxOrderId === null) {
         maxOrderId = 1;
         await db.query('ALTER TABLE invoices AUTO_INCREMENT = 1')
--- mebmarket/pages/api/getcount.js
+++ mebmarket/pages/api/getcount.js
@@ -1,8 +1,9 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import excuteQuery from "@/shared/database";
 
 export default async function handler(req, res) {
-    const count = await prisma.book.count();
-    res.json(count);
+    const count = await excuteQuery({
+        query: "SELECT COUNT(bookId) AS total FROM `book`"
+    })
+    console.log(count[0].total)
+    res.json(count[0].total);
 }
