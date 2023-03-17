582335f0aafeb0eabb34dbe917383b1115516d2e
Nonz007x <comandor969@gmail.com>
2023-03-17 16:04:48 +0700

    remove getcategory

--- mebmarket/pages/api/getcategory.js
+++
@@ -1,6 +0,0 @@
-import excuteQuery from "@/shared/database";
-
-export default async function handleChange(req,res) {
-    const Sql = await excuteQuery({query:"select * from category"});
-    res.send(Sql);
-}
